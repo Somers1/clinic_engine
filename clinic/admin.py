fd8d7066bf820f47822e701201524410c9563b32
jake_somerville <jake.somerville@lynxx.com>
2022-01-29 15:32:06 +1100

    + register to admin

--- clinic/admin.py
+++ clinic/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from clinic.models import Clinic, School, Student, Assessment
 
 # Register your models here.
+admin.site.register(Clinic)
+admin.site.register(School)
+admin.site.register(Student)
+admin.site.register(Assessment)
